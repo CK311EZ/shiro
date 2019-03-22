c492ffd074e731992e2d3d2c41bfa450d7cc5de3
chenk <chenkang@belink.com>
2019-03-22 09:46:26 +0800

    修改测试添加1

--- src/main/java/com/shop/shiro/bean/testBean.java
+++ src/main/java/com/shop/shiro/bean/testBean.java
@@ -1,4 +1,22 @@
 package com.shop.shiro.bean;
 
-public class testBean {
+public class testBean{
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
 }
